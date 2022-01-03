1319a4a98494b0c5ff9049e5cd3cfd50a71e0201
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-03 15:20:42 -0300

    Em desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import "./App.css"
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import ChatIcon from "@material-ui/icons/Chat"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import SearchIcon from "@material-ui/icons/Search"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import ChatListItem from "./components/ChatListItem"
+
+export default () => {
+
+  const [chatList, setChatList] = useState([{}, {}, {}, {}])
+
+  return(
+    <div className="app-window">
+      <div className="sidebar">
+
+        <header>
+          <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: "#919191"}} />
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{color: "#919191"}} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: "#919191"}} />
+            </div>
+          </div>
+        </header>
+
+        <div className="search">
+          <div className="search--input">
+            <SearchIcon fontSize="small" style={{color: "#919191"}} />
+            <input type="search" placeholder="Procurar ou comecar uma nova conversa" /> 
+          </div>
+        </div>
+
+        <div className="chatlist">
+          {chatList.map((item, key) => (
+            <ChatListItem key={key} />
+          ))}
+        </div>
+
+      </div>
+
+      <div className="contentarea">
+        ...
+      </div>
+
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/ChatListItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./ChatListItem.css"
+
+export default () => {
+    return(
+        <div className="chatListItem">
+            <img className="chatListItem--avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+            <div className="chatListItem--lines">
+                <div className="chatListItem--line">
+                    <div className="chatListItem--name">Thomas Schmitz</div>
+                    <div className="chatListItem--date">19:00</div>
+                </div>
+                <div className="chatListItem--line">
+                    <div className="chatListItem--lastMsg">
+                        <p>Opa, tudo bem?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
