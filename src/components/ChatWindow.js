b1da9e9fae46b83fc0e5e479e19a2c34e8fe1796
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-04 18:47:04 -0300

    Desenvolvimento

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -3,8 +3,18 @@ import "./ChatWindow.css";
 import SearchIcon from "@material-ui/icons/Search"
 import AttachFileIcon from "@material-ui/icons/AttachFile"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
+import CloseIcon from "@material-ui/icons/Close"
+import SendIcon from "@material-ui/icons/Send"
+import MicIcon from "@material-ui/icons/Mic"
+import EmojiPicker from "emoji-picker-react"
 
 export default () => {
+
+    const handleEmojiClick = () => {
+
+    }
+
     return(
         <div className="chatWindow">
             <div className="chatWindow--header">
@@ -29,8 +39,28 @@ export default () => {
             <div className="chatWindow--body">
 
             </div>
-            <div className="chatWindow--footer">
 
+            <div className="chatWindow--emojiarea">
+                <EmojiPicker disableSearchBar disableSkinTonePicker onEmojiClick={handleEmojiClick} />
+            </div>
+
+            <div className="chatWindow--footer">
+                <div className="chatWindow--pre">
+                    <div className="chatWindow--btn">
+                        <CloseIcon style={{color: "#919191"}} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <InsertEmoticonIcon style={{color: "#919191"}} />
+                    </div>
+                </div>
+                <div className="chatWindow--inputarea">
+                    <input className="chatWindow--input" type="text" placeholder="Digite uma mensagem" />
+                </div>
+                <div className="chatWindow--pos">
+                    <div className="chatWindow--btn">
+                        <SendIcon style={{color: "#919191"}} />
+                    </div>
+                </div>
             </div>
         </div>
     )
