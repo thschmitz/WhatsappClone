92dd00026430a78962969dd534f5bc7731d52cd5
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-05 21:40:25 -0300

    Desenvolvimento

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
-import firebase from "@firebase/app";
-import "firebase/firebase-auth"
-import "firebase/firebase-firestore"
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 
 import firebaseConfig from "./firebaseConfig"
 
